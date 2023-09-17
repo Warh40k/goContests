2c48c0ddb1fa1f56429e33382bc6708502624a9d
nikita <nicita.zinkevitch@yandex.ru>
2023-09-17 21:50:15 +0300

    Aisd lr1 F tasks F and G

--- aisd/lr1/F/main.go
+++ aisd/lr1/F/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"math/rand"
 	"os"
 )
@@ -16,31 +17,35 @@ func main() {
 	for i := 0; i < N; i++ {
 		fmt.Fscan(in, &X[i])
 	}
-	//fmt.Fprintln(out, sleepDay(N, X))
 	test(out)
+	fmt.Fprintln(out, sleepDayTest(N, X))
 	out.Flush()
 }
 
 func test(out *bufio.Writer) {
 	for {
 		//N := rand.Intn(8*int(math.Pow(10, 6))) + 1
-		N := rand.Intn(20) + 1
+		N := 8 * int(math.Pow(10, 6))
 		X := make([]int, N)
 		for i := range X {
-			X[i] = rand.Intn(20) - 10 + 1
+			X[i] = rand.Intn(2000) - 1000 + 1
 		}
-		a := sleepDay(N, X)
+		//a := sleepDay(N, X)
 		b := sleepDayTest(N, X)
-		if a != b {
-			fmt.Fprintln(out, X, a, b)
-		}
+		//if a != b {
+		//	fmt.Fprintln(out, X, a, b)
+		//}
+		fmt.Fprintln(out, b)
+		out.Flush()
 	}
 
 }
 
 func sleepDay(N int, X []int) int {
 	var sbackward, sforward = X[0], 0
-
+	if N == 1 {
+		return 0
+	}
 	for i := 1; i < N; i++ {
 		for j := i + 1; j < N; j++ {
 			sforward += X[j]
@@ -63,10 +68,7 @@ func sleepDayTest(N int, X []int) int {
 
 	for i := 1; i < N; i++ {
 		sforward[i] = sforward[i-1] + X[i]
-	}
-
-	for i := N - 2; i > 0; i-- {
-		sbackward[i] = sbackward[i+1] + X[i]
+		sbackward[N-i-1] = sbackward[N-i] + X[N-i-1]
 	}
 
 	for i := 0; i < N; i++ {
--- aisd/lr1/G/main.go
+++ aisd/lr1/G/main.go
@@ -1,1 +1,45 @@
-package F
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	in, out := bufio.NewReader(os.Stdin), bufio.NewWriter(os.Stdout)
+
+	var t, n int
+	fmt.Fscan(in, &t)
+	var votes [][]string
+
+	for i := 0; i < t; i++ {
+		fmt.Fscan(in, &n)
+		votes = make([][]string, n)
+		for j := 0; j < n; j++ {
+			votes[j] = make([]string, 2)
+			fmt.Fscan(in, &votes[j][0], &votes[j][1])
+		}
+		findCompomiss(out, n, votes)
+	}
+
+	out.Flush()
+}
+
+func findCompomiss(out *bufio.Writer, n int, votes [][]string) {
+	var vmin, vmax, val = 15, 30, 0
+	for _, vote := range votes {
+		val, _ = strconv.Atoi(vote[1])
+		if vote[0] == ">=" && vmin < val {
+			vmin = val
+		} else if vote[0] == "<=" && vmax > val {
+			vmax = val
+		}
+		if vmin > vmax {
+			fmt.Fprintln(out, -1)
+		} else {
+			fmt.Fprintln(out, vmin)
+		}
+	}
+}
