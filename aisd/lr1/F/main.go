2eb8f1ec49cece7a14018b9f52f73ff9f555a2ae
nikita <nicita.zinkevitch@yandex.ru>
2023-09-17 15:51:13 +0300

    Aisd lr1 F task second attempt (fix errors)

--- aisd/lr1/F/main.go
+++ aisd/lr1/F/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math/rand"
 	"os"
 )
 
@@ -15,11 +16,47 @@ func main() {
 	for i := 0; i < N; i++ {
 		fmt.Fscan(in, &X[i])
 	}
-	fmt.Fprintln(out, sleepDay(N, X))
+	//fmt.Fprintln(out, sleepDay(N, X))
+	test(out)
 	out.Flush()
 }
 
+func test(out *bufio.Writer) {
+	for {
+		//N := rand.Intn(8*int(math.Pow(10, 6))) + 1
+		N := rand.Intn(20) + 1
+		X := make([]int, N)
+		for i := range X {
+			X[i] = rand.Intn(20) - 10 + 1
+		}
+		a := sleepDay(N, X)
+		b := sleepDayTest(N, X)
+		if a != b {
+			fmt.Fprintln(out, X, a, b)
+		}
+	}
+
+}
+
 func sleepDay(N int, X []int) int {
+	var sbackward, sforward = X[0], 0
+
+	for i := 1; i < N; i++ {
+		for j := i + 1; j < N; j++ {
+			sforward += X[j]
+			if sforward > sbackward {
+				break
+			} else if sbackward == sforward {
+				return i
+			}
+		}
+		sbackward += X[i]
+		sforward = 0
+	}
+	return -1
+}
+
+func sleepDayTest(N int, X []int) int {
 	sforward, sbackward := make([]int, N), make([]int, N)
 	sforward[0] = X[0]
 	sbackward[N-1] = X[N-1]
