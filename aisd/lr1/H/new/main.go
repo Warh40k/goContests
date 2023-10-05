954efec8af83563a64dba36637126e13734d1bbb
nikita <nicita.zinkevitch@yandex.ru>
2023-10-05 22:56:13 +0300

    Press f

--- aisd/lr1/H/new/main.go
+++ aisd/lr1/H/new/main.go
@@ -10,7 +10,9 @@ import (
 
 func main() {
 	in, out := bufio.NewReader(os.Stdin), bufio.NewWriter(os.Stdout)
-	test(out)
+	//testHard(out)
+	//os.Exit(0)
+	testCorrect()
 	os.Exit(0)
 	var N int
 	fmt.Fscan(in, &N)
@@ -19,44 +21,51 @@ func main() {
 		fmt.Fscan(in, &X[i])
 	}
 	count := countBorders(N, X)
+	//countOld, _ := countBordersOld(N, X)
 	fmt.Fprintln(out, count)
 	out.Flush()
 }
 
 func countBorders(N int, X []int) int {
 	count := 0
-	marked := make([]int, N)
+	marked := make([][]int, 2)
+	marked[0] = make([]int, N)
+	marked[1] = make([]int, N)
+
+	table := make([][]int, N)
+	for i := 0; i < N; i++ {
+		table[i] = make([]int, N)
+		for j := 0; j < N; j++ {
+			table[i][j] = X[i] & X[j]
+		}
+	}
 	for k := 1; k < N; k++ {
-		counter, success := 0, false
+		counter := 0
 
 		for i, j := 0, k; i < N; i, j = i+1, j+1 {
 			if j == N {
 				j = 0
 			}
 
-			if j == 1e5 {
-				fmt.Print("Capez")
-			}
 			if X[i]&X[j] != 0 {
 				counter++
-				if k == 1 || (marked[i] == 0 && marked[(i+N+1)%N] == 0) && success == false {
+				if k == 1 {
 					count++
-					success = true
-				} else {
-					success = false
+				} else if marked[0][i] == 0 && X[(i+N+1)%N]&X[j] == 0 {
+					count++
+					marked[0][(i+N+1)%N] = 1
 				}
-				marked[i] = 1
-			} else if k != 1 && (marked[i] != 0 || marked[(i+N+1)%N] != 0) {
-				marked[i] = 1
+
+				marked[0][i] = 1
+			} else if k != 1 && (marked[0][i] != 0 || marked[0][(i+N+1)%N] != 0) {
+				marked[0][i] = 1
 				counter++
-				success = false
-			} else {
-				marked[i] = 0
 			}
 		}
 		if counter >= N {
 			break
 		}
+		//marked[0], marked[1] = marked[1], marked[0]
 	}
 
 	if count == 0 {
@@ -64,49 +73,8 @@ func countBorders(N int, X []int) int {
 	}
 	return count
 }
-func hasBorders(start, end, N int, table [][]int) bool {
-	for k := end - 1; k != start; k-- {
-		if k == -1 {
-			k = N - 1
-		}
-		i := start
-		j := k
-		for j != end+1 {
-			if i == N {
-				i = 0
-			}
-			if j == N {
-				j = 0
-			}
-			if table[i][j] != 0 {
-				return true
-			}
-			i++
-			j++
-		}
-	}
-	return false
-	//for i != end {
-	//	j := end - 1
-	//
-	//	for j != i {
-	//		if j == -1 {
-	//			j = N - 1
-	//		}
-	//		if table[i][j] != 0 {
-	//			return j
-	//		}
-	//		j--
-	//	}
-	//	if i == N-1 {
-	//		i = 0
-	//	} else {
-	//		i++
-	//	}
-	//}
-}
 
-func test(writer *bufio.Writer) {
+func testHard(writer *bufio.Writer) {
 	N := int(4 * math.Pow(10, 5))
 	arr := make([]int, N)
 
@@ -116,4 +84,55 @@ func test(writer *bufio.Writer) {
 	count := countBorders(N, arr)
 	fmt.Fprintln(writer, count)
 	writer.Flush()
+}
+
+func testCorrect() {
+	for {
+		N := rand.Intn(5) + 1
+		arr := make([]int, N)
+
+		for i := 0; i < N; i++ {
+			arr[i] = rand.Intn(10) + 1
+		}
+		count1, _ := countBordersOld(N, arr)
+		count2 := countBorders(N, arr)
+
+		if count1 != count2 {
+			fmt.Println(count1, count2, arr)
+			return
+		}
+		//fmt.Println("ok")
+	}
+}
+
+func countBordersOld(N int, X []int) (int, []int) {
+	count := 0
+	var borders []int
+	for i := 1; i < N; i++ {
+		for j := 0; j < N; j++ {
+			var next = j + i
+			if next > N-1 {
+				next -= N
+			}
+			if (i == 1 || hasBordersBetween(j, i, N, borders)) && X[j]&X[next] != 0 {
+				borders = append(borders, next)
+				count++
+			}
+		}
+	}
+	if count == 0 {
+		count = 1
+	}
+	return count, borders
+}
+func hasBordersBetween(start, step, N int, borders []int) bool {
+	if borders != nil {
+		for _, val := range borders {
+			if (start+step <= N-1 && val > start && val <= start+step) ||
+				(start+step > N-1 && (val > start || val <= start+step-N)) {
+				return false
+			}
+		}
+	}
+	return true
 }
